2c0936c736ec4800ac878d805c9fb8a70825cd84
DESKTOP-4EFEVVD\jrsar <jr.sarcilla07@gmail.com>
2023-08-09 23:13:31 +0800

    style(design_system): update colors

--- src/components/Color/Color.tsx
+++ src/components/Color/Color.tsx
@@ -35,6 +35,13 @@ const Color = () => {
             <p className="text-[20px] font-[700] leading-[32px] tracking-[-0.1px] text-[#ffffff] opacity-80">E5E5E5</p>
           </div>
         </div>
+        <div className="flex flex-col gap-8">
+          <div className="h-[128px] w-[128px] bg-base-05" />
+          <div className="flex flex-col gap-4">
+            <h3 className="text-[24px] font-[700] leading-8 tracking-[-0.12px] text-[#ffffff] opacity-80">Base 05</h3>
+            <p className="text-[20px] font-[700] leading-[32px] tracking-[-0.1px] text-[#ffffff] opacity-80">D4D4D4</p>
+          </div>
+        </div>
       </div>
     </>
   )
