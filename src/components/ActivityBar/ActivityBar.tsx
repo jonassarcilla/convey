c2959b0ca1643461fa250070d1177a5ae39c2735
DESKTOP-4EFEVVD\jrsar <jr.sarcilla07@gmail.com>
2023-08-28 17:08:23 +0800

    style(build): gitignore

---
+++ src/components/ActivityBar/ActivityBar.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+// export interface IActivityBarProps {}
+// {}: IActivityBarProps
+// This is a compound components
+// Activate figma
+const ActivityBar = () => {
+  return <></>
+}
+
+export default React.memo(ActivityBar)
